b15a20e7fd44dc8025708341553669f3a23756a1
Rubylium <alexisoko0@gmail.com>
2020-12-08 15:37:08 +0100

    Create LoginForm.cs

---
+++ PPE-LIGUE-DAO/LoginForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PPE_LIGUE_DAO
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Login Réussi!");
+            main fm = new main();
+
+
+            this.Hide();
+            fm.Show();
+        }
+    }
+}
