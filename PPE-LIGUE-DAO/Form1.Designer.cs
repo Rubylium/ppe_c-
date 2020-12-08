bfac02b5892cd29fd71300f7b29394e6e37b2c8a
Rubylium <alexisoko0@gmail.com>
2020-12-08 15:22:59 +0100

    Init

---
+++ PPE-LIGUE-DAO/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PPE_LIGUE_DAO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelMdp = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(102, 88);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.Location = new System.Drawing.Point(102, 22);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(100, 13);
+            this.labelLogin.TabIndex = 2;
+            this.labelLogin.Text = "Login";
+            // 
+            // labelMdp
+            // 
+            this.labelMdp.Location = new System.Drawing.Point(102, 72);
+            this.labelMdp.Name = "labelMdp";
+            this.labelMdp.Size = new System.Drawing.Size(100, 13);
+            this.labelMdp.TabIndex = 3;
+            this.labelMdp.Text = "Mots de passe";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(99, 114);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(103, 23);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "Se connecter";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 174);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.labelMdp);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Géstion stands";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button buttonLogin;
+
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelMdp;
+
+        private System.Windows.Forms.TextBox textBox2;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        #endregion
+    }
+}
+
---
+++ PPE-LIGUE-DAO/main.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace PPE_LIGUE_DAO
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+    }
+}
