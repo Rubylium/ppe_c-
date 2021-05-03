577b77ec22d61e52595cdccce83154913ac45887
Rubylium <alexisoko0@gmail.com>
2021-05-03 15:41:40 +0200

    Update main.cs

--- PPE-LIGUE-DAO/main.cs
+++ PPE-LIGUE-DAO/main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
@@ -11,20 +12,42 @@ namespace PPE_LIGUE_DAO
             InitializeComponent();
         }
 
-        public void GetAllPartenaire()
+        public List<Partenaire> GetAllPartenaire()
         {
             DAOFactory db = new DAOFactory();
             db.connecter();
             string req = "SELECT * FROM partenaire";
-
             SqlDataReader reader = db.excecSQLRead(req);
 
+            List<Partenaire> lesPartenaires = new List<Partenaire>();
+            
             while (reader.Read())
             {
-                
+                Partenaire part = new Partenaire(Int32.Parse(reader[0].ToString()), reader[1].ToString());
+                lesPartenaires.Add(part);
             }
 
             db.deconnecter();
+            return lesPartenaires;
+        }
+        
+        public List<TypePartenaire> GetAllTypePartenaire()
+        {
+            DAOFactory db = new DAOFactory();
+            db.connecter();
+            string req = "SELECT * FROM typePartenaire";
+            SqlDataReader reader = db.excecSQLRead(req);
+
+            List<TypePartenaire> lesTypes = new List<TypePartenaire>();
+            
+            while (reader.Read())
+            {
+                TypePartenaire part = new TypePartenaire(Int32.Parse(reader[0].ToString()), reader[1].ToString());
+                TypePartenaire.Add(part);
+            }
+
+            db.deconnecter();
+            return lesTypes;
         }
     }
 }
